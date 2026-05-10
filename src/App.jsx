26e3d9f135eabb43c741faf51932b15de278f6f3
niwesh-waiba <niweshwaiba@gmail.com>
2026-05-10 19:10:34 +0545

    final COmmit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,26 @@ const pageVariants = {
 };
 
 export default function App() {
-  const [page, setPage] = useState("home");
+  const [page, setPage] = useState(() => {
+    const hash = window.location.hash.replace("#", "");
+    return hash || "home";
+  });
+
+  useEffect(() => {
+    const handlePopState = () => {
+      const hash = window.location.hash.replace("#", "");
+      setPage(hash || "home");
+    };
+    window.addEventListener("popstate", handlePopState);
+    return () => window.removeEventListener("popstate", handlePopState);
+  }, []);
+
+  const navigateTo = (newPage) => {
+    const currentHash = window.location.hash.replace("#", "") || "home";
+    if (newPage === currentHash) return;
+    window.history.pushState(null, "", `#${newPage}`);
+    setPage(newPage);
+  };
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -24,20 +43,20 @@ export default function App() {
 
   const renderPage = () => {
     switch (page) {
-      case "home":          return <HomePage    setPage={setPage} />;
-      case "about":         return <AboutBusinessPage setPage={setPage} />;
+      case "home":          return <HomePage    setPage={navigateTo} />;
+      case "about":         return <AboutBusinessPage setPage={navigateTo} />;
       case "contact":       return <ContactPage />;
-      case "garage-detail": return <GarageCafeDetailPage setPage={setPage} />;
-      case "juice-detail":  return <GarageJuiceDetailPage setPage={setPage} />;
-      case "bubble-detail": return <LukaBubbleTeaDetailPage setPage={setPage} />;
-      case "barista-detail": return <GarageBaristaSchoolDetailPage setPage={setPage} />;
-      default:              return <HomePage    setPage={setPage} />;
+      case "garage-detail": return <GarageCafeDetailPage setPage={navigateTo} />;
+      case "juice-detail":  return <GarageJuiceDetailPage setPage={navigateTo} />;
+      case "bubble-detail": return <LukaBubbleTeaDetailPage setPage={navigateTo} />;
+      case "barista-detail": return <GarageBaristaSchoolDetailPage setPage={navigateTo} />;
+      default:              return <HomePage    setPage={navigateTo} />;
     }
   };
 
   return (
     <>
-      <Nav page={page} setPage={setPage} />
+      <Nav page={page} setPage={navigateTo} />
       {/* offset for fixed nav (100px height) */}
       <div style={{ paddingTop: 100 }}>
         <AnimatePresence mode="wait">
